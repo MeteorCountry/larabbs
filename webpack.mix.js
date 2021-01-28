1eca975f9ae62ac8aff5ec43bd22df530239009e
lxg <liuxiaoguolxg@163.com>
2021-01-28 23:12:40 +0800

    增加字体图标

--- webpack.mix.js
+++ webpack.mix.js
@@ -10,6 +10,6 @@ const mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
-
+// 已经处理静态文件浏览器缓存问题，修改此文件后需要 npm run watch-poll
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css').version();
