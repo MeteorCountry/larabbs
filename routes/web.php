58a887731ee1b2da1ed6aa88a6c6655b3c0bc068
lxg <liuxiaoguolxg@163.com>
2021-02-01 11:09:36 +0800

    生成话题骨架

--- routes/web.php
+++ routes/web.php
@@ -68,3 +68,5 @@ Route::post('email/resend', 'Auth\VerificationController@resend')->name('verific
 
 # User
 Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
+
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
