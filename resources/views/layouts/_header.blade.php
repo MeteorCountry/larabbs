01747a73daacb4d61d050ef5ec76b9e0bddb32a1
lxg <liuxiaoguolxg@163.com>
2021-02-01 22:54:36 +0800

    分类话题列表

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -1,28 +1,3 @@
-{{--<nav class="navbar navbar-expand-lg navbar-light bg-light navbar-static-top">--}}
-{{--  <div class="container">--}}
-{{--    <!-- Branding Image -->--}}
-{{--    <a class="navbar-brand " href="{{ url('/') }}">--}}
-{{--      LaraBBS--}}
-{{--    </a>--}}
-{{--    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">--}}
-{{--      <span class="navbar-toggler-icon"></span>--}}
-{{--    </button>--}}
-
-{{--    <div class="collapse navbar-collapse" id="navbarSupportedContent">--}}
-{{--      <!-- Left Side Of Navbar -->--}}
-{{--      <ul class="navbar-nav mr-auto">--}}
-
-{{--      </ul>--}}
-
-{{--      <!-- Right Side Of Navbar -->--}}
-{{--      <ul class="navbar-nav navbar-right">--}}
-{{--        <!-- Authentication Links -->--}}
-{{--        <li class="nav-item"><a class="nav-link" href="{{ route('login') }}">登录</a></li>--}}
-{{--        <li class="nav-item"><a class="nav-link" href="{{ route('register') }}">注册</a></li>--}}
-{{--      </ul>--}}
-{{--    </div>--}}
-{{--  </div>--}}
-{{--</nav>--}}
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light navbar-static-top">
   <div class="container">
@@ -37,7 +12,11 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <!-- Left Side Of Navbar -->
       <ul class="navbar-nav mr-auto">
-
+        <li class="nav-item {{ active_class(if_route('topics.index')) }}"><a class="nav-link" href="{{ route('topics.index') }}">话题</a></li>
+        <li class="nav-item {{ category_nav_active(1) }}"><a class="nav-link" href="{{ route('categories.show', 1) }}">分享</a></li>
+        <li class="nav-item {{ category_nav_active(2) }}"><a class="nav-link" href="{{ route('categories.show', 2) }}">教程</a></li>
+        <li class="nav-item {{ category_nav_active(3) }}"><a class="nav-link" href="{{ route('categories.show', 3) }}">问答</a></li>
+        <li class="nav-item {{ category_nav_active(4) }}"><a class="nav-link" href="{{ route('categories.show', 4) }}">公告</a></li>
       </ul>
 
       <!-- Right Side Of Navbar -->
