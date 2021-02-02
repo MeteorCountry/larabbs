d50a8ae234531c2c1775bfdd422d41123663da46
lxg <liuxiaoguolxg@163.com>
2021-02-02 21:47:48 +0800

    翻译 URL 标识

--- routes/web.php
+++ routes/web.php
@@ -69,7 +69,8 @@ Route::post('email/resend', 'Auth\VerificationController@resend')->name('verific
 # User
 Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
 
-Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'create', 'store', 'update', 'edit', 'destroy']]);
+Route::get('topics/{topic}/{slug?}', 'TopicsController@show')->name('topics.show');
 
 Route::resource('categories','CategoriesController',['only' => ['show']]);
 
