e4e116faca9f89fa916fe325958dd94cbf6399ea
lxg <liuxiaoguolxg@163.com>
2021-02-03 21:35:35 +0800

    用户可以发表评论

--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -2,6 +2,7 @@
 
 namespace App\Observers;
 
+use App\Http\Requests\Request;
 use App\Models\Reply;
 
 // creating, created, updating, updated, saving,
@@ -9,13 +10,14 @@ use App\Models\Reply;
 
 class ReplyObserver
 {
+    public function created(Reply $reply)
+    {
+        $reply->topic->reply_count = $reply->topic->replies->count();
+        $reply->topic->save();
+    }
     public function creating(Reply $reply)
     {
-        //
-    }
-
-    public function updating(Reply $reply)
-    {
-        //
+//        暂不处理 xss
+//        $reply->content = clean($reply->content, 'user_topic_body');
     }
 }
